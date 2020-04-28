84097ffa0cd1b6fa3788b0e43118619017f6b4f2
Innocent <55434969+Wiiinterrr@users.noreply.github.com>
2020-04-28 03:40:12 -0700

    portfolio

---
+++ main.js
@@ -0,0 +1,29 @@
+function blue(){
+    $('#name').css({
+        'color': 'blue'
+    });
+}
+
+function red(){
+    $('#name').css({
+        'color': 'red'
+    });
+}
+
+function black(){
+    $('#name').css({
+        'color': 'black'
+    });
+}
+
+function green(){
+    $('#name').css({
+        'color': 'green'
+    });
+}
+
+function yellow(){
+    $('#name').css({
+        'color': 'yellow'
+    });
+}
